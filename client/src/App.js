5f2df923515d664a36c19501fe776c13f0e83e04
Georgii Gerbst <Georgii_Gerbst@epam.com>
2020-02-25 20:53:50 +0300

    added spinner on loading

--- client/src/App.js
+++ client/src/App.js
@@ -4,84 +4,35 @@ import './App.css';
 import Spinner from './Spinner';
 
 const Component = () => {
-
-  const [url, setUrl] = useState('');
   const [loading, setLoading] = useState(false);
-  // const newWindow = window.open();
-
-  const showFile = (blob, newWindow) => {
-    const newBlob = new Blob([blob], {
-      type: 'application/pdf',
-    });
-    const data = window.URL.createObjectURL(newBlob);
-
-    setUrl(data);
-
-    // console.log(newWindow);
-
-    // newWindow.location = data;
-    // window.focus();
-  };
-
+  
   const handleLinkClick = () => {
-    axios.get("/api/file",
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/pdf'
-                },
-                responseType: "blob"
-            }).then((response) => {
-                console.log(response.data);
-                
-                const blob = new Blob([response.data], { type: 'application/pdf' })
-                const objectUrl = window.URL.createObjectURL(blob);
-                // debugger
-                window.open(objectUrl)
-            }).catch((error) => { alert(error) })
-    
-    // const newWindow = window.open();
-    // fetch('../189838.pdf')
-    // fetch('/api/file', {
-    //   responseType: 'arraybuffer',
-    //   headers: {
-    //     'Accept': 'application/pdf'
-    //   }
-    // })
-    // .then(response => {
-    //   console.log(response);
-    //   // response.data is an empty object
-    //   const blob = new Blob([response], {
-    //     type: 'application/pdf',
-    //   });
-    //   const data = window.URL.createObjectURL(blob);
-    //   window.open(data);
-    //   // FileSaver.saveAs(blob, Math.random());
-    // })
-    // .then((response) => {
-    //   const newBlob = new Blob([response], {
-    //     type: 'application/pdf',
-    //   });
-    //   const data = window.URL.createObjectURL(newBlob);
-  
-    //   // setUrl(data);
-
-    //   console.log(data);
+    setLoading(true);
+    axios.get("/api/file", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/pdf',
+      },
+      responseType: "blob",
+    })
+    .then((response) => {
+      setLoading(false);
+      const blob = new Blob([response.data], { type: 'application/pdf' })
+      const objectUrl = window.URL.createObjectURL(blob);
+      setTimeout(() => {
+        window.open(objectUrl)
+      }, 2000);
       
-      
-    //   newWindow.location = data;
-  
-    //   // console.log(newWindow);
-  
-    //   // newWindow.location = data;
-    //   // window.focus();
-    // })
+    })
+    .catch((error) => { alert(error) })
   }
  
   return (
     <div className="App">
       <a id='clickMe' onClick={handleLinkClick}>Click Me!</a>
-      <Spinner />
+      {
+        loading && <Spinner />
+      }
     </div>
   );
 }
